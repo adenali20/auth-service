946c871cbc6164bdb14564ecda84e82e6e6d7e51
aden <adenali362@gmail.com>
2025-12-30 14:43:56 -0600

    disable cors

--- src/main/java/com/adenali/fms/config/SecurityConfig.java
+++ src/main/java/com/adenali/fms/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -22,37 +23,16 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.sessionManagement(sessionConfig -> sessionConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .cors(corseConfig->corseConfig.configurationSource(new CorsConfigurationSource() {
-                    @Override
-                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-                        CorsConfiguration config = new CorsConfiguration();
-                        config.setAllowedOrigins(List.of(
-                                "http://localhost:3000",
-                                "http://localhost:8080",
-                                "http://138.68.247.139:3000",
-                                "http://138.68.247.139:8080",
-                                "http://dev.adenali.com:3000",
-                                "https://dev.adenali.com",
-                                "http://dev.adenali.com:8080",
-                                "http://dev.adenali.com:3000",
-                                "http://dev.adenali.com",
-                                "http://adenali.com",
-                                "http://10.0.0.167:3000"));
-                        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-                        config.setAllowedHeaders(List.of("*"));
-                        config.setAllowCredentials(true);
-                        return config;
-                    }
-                }))
-                .csrf(hcsrf -> hcsrf.disable())
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
 //                                .requestMatchers("api/users/salary").hasRole("ADMIN")
                                 .requestMatchers("/api/authservice/user/signup").permitAll()
                                 .requestMatchers("/api/authservice/user/login").permitAll()
                                 .anyRequest().authenticated()
                 )
-                .formLogin(flc-> flc.disable())
-                .httpBasic(htc -> htc.disable());
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable);
         return http.build();
 
     }
